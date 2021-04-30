d056290dfbd397f1bc1003a5073141cb13316cd6
Timsal Zehra <60501856+Timsal123@users.noreply.github.com>
2021-05-01 05:26:49 +0530

    Add files via upload

---
+++ NationalPer.sql
@@ -0,0 +1,281 @@
+﻿/* CREATE TABLE */
+CREATE TABLE IF NOT EXISTS states1(
+    statename VARCHAR(100), 
+    Area DECIMAL(10, 2), 
+    nationalshare DECIMAL(10, 2)
+);
+/* INSERT QUERY */
+INSERT INTO states1(
+    statename, 
+    Area, 
+    nationalshare
+) 
+VALUES 
+  ('Rajasthan', 342239, 10.55);
+/* INSERT QUERY */
+INSERT INTO states1(
+    statename, 
+    Area, 
+    nationalshare
+) 
+VALUES 
+  ('Madhya Pradesh', 308350, 9.37);
+/* INSERT QUERY */
+INSERT INTO states1(
+    statename, 
+    Area, 
+    nationalshare
+) 
+VALUES 
+  ('Maharashtra', 307713, 9.36);
+/* INSERT QUERY */
+INSERT INTO states1(
+    statename, 
+    Area, 
+    nationalshare
+) 
+VALUES 
+  ('Uttar Pradesh', 240928, 7.33);
+/* INSERT QUERY */
+INSERT INTO states1(
+    statename, 
+    Area, 
+    nationalshare
+) 
+VALUES 
+  ('Gujarat', 196024, 5.96);
+/* INSERT QUERY */
+INSERT INTO states1(
+    statename, 
+    Area, 
+    nationalshare
+) 
+VALUES 
+  ('Karnataka', 191791, 5.83);
+/* INSERT QUERY */
+INSERT INTO states1(
+    statename, 
+    Area, 
+    nationalshare
+) 
+VALUES 
+  ('Andhra Pradesh', 162970, 4.87);
+/* INSERT QUERY */
+INSERT INTO states1(
+    statename, 
+    Area, 
+    nationalshare
+) 
+VALUES 
+  ('Odisha', 155707, 4.73);
+/* INSERT QUERY */
+INSERT INTO states1(
+    statename, 
+    Area, 
+    nationalshare
+) 
+VALUES 
+  ('Chhattisgarh', 135191, 4.11);
+/* INSERT QUERY */
+INSERT INTO states1(
+    statename, 
+    Area, 
+    nationalshare
+) 
+VALUES 
+  ('Tamil Nadu', 130058, 3.95);
+/* INSERT QUERY */
+INSERT INTO states1(
+    statename, 
+    Area, 
+    nationalshare
+) 
+VALUES 
+  ('Telangana', 112077, 3.49);
+/* INSERT QUERY */
+INSERT INTO states1(
+    statename, 
+    Area, 
+    nationalshare
+) 
+VALUES 
+  ('Bihar', 94163, 2.86);
+/* INSERT QUERY */
+INSERT INTO states1(
+    statename, 
+    Area, 
+    nationalshare
+) 
+VALUES 
+  ('West Bengal', 88752, 2.7);
+/* INSERT QUERY */
+INSERT INTO states1(
+    statename, 
+    Area, 
+    nationalshare
+) 
+VALUES 
+  ('Arunachal Pradesh', 83743, 2.54);
+/* INSERT QUERY */
+INSERT INTO states1(
+    statename, 
+    Area, 
+    nationalshare
+) 
+VALUES 
+  ('Jharkhand', 79714, 2.42);
+/* INSERT QUERY */
+INSERT INTO states1(
+    statename, 
+    Area, 
+    nationalshare
+) 
+VALUES 
+  ('Assam', 78438, 2.38);
+/* INSERT QUERY */
+INSERT INTO states1(
+    statename, 
+    Area, 
+    nationalshare
+) 
+VALUES 
+  ('Ladakh', 59146, 1.8);
+/* INSERT QUERY */
+INSERT INTO states1(
+    statename, 
+    Area, 
+    nationalshare
+) 
+VALUES 
+  ('Himachal Pradesh', 55673, 1.7);
+/* INSERT QUERY */
+INSERT INTO states1(
+    statename,
+    Area, 
+    nationalshare
+) 
+VALUES 
+  ('Uttarakhand', 53483, 1.62);
+/* INSERT QUERY */
+INSERT INTO states1(
+    statename,
+    Area, 
+    nationalshare
+) 
+VALUES 
+  ('Punjab', 50362, 1.53);
+/* INSERT QUERY */
+INSERT INTO states1(
+    statename, 
+    Area, 
+    nationalshare
+) 
+VALUES 
+  ('Haryana', 44212, 1.34);
+/* INSERT QUERY */
+INSERT INTO states1(
+    statename, 
+    Area, 
+    nationalshare
+) 
+VALUES 
+  ('Jammu and Kashmir', 42241, 1.28);
+/* INSERT QUERY */
+INSERT INTO states1(
+    statename, 
+    Area, 
+    nationalshare
+) 
+VALUES 
+  ('Kerala', 38863, 1.18);
+/* INSERT QUERY */
+INSERT INTO states1(
+    statename, 
+    Area, 
+    nationalshare
+) 
+VALUES 
+  ('Meghalaya', 22429, 0.68);
+/* INSERT QUERY */
+INSERT INTO states1(
+    statename, 
+    Area, 
+    nationalshare
+) 
+VALUES 
+  ('Manipur', 22327, 0.68);
+/* INSERT QUERY */
+INSERT INTO states1(
+    statename, 
+    Area, 
+    nationalshare
+) 
+VALUES 
+  ('Mizoram', 21081, 0.64);
+/* INSERT QUERY */
+INSERT INTO states1(
+    statename, 
+    Area, 
+    nationalshare
+) 
+VALUES 
+  ('Nagaland', 16579, 0.5);
+/* INSERT QUERY */
+INSERT INTO states1(
+    statename, 
+    Area, 
+    nationalshare
+) 
+VALUES 
+  ('Tripura', 10486, 0.31);
+/* INSERT QUERY */
+INSERT INTO states1(
+    statename, 
+    Area, 
+    nationalshare
+) 
+VALUES 
+  ('Sikkim', 7096, 0.21);
+/* INSERT QUERY */
+INSERT INTO states1(
+    statename, 
+    Area, 
+    nationalshare
+) 
+VALUES 
+  ('Goa', 3702, 0.11);
+/* INSERT QUERY */
+INSERT INTO states1(
+    statename, 
+    Area, 
+    nationalshare
+) 
+VALUES 
+  ('Delhi', 1483, 0.04);
+/* INSERT QUERY */
+INSERT INTO states1(
+    statename, 
+    Area, 
+    nationalshare
+) 
+VALUES 
+  (
+    'Dadra and Nagar Haveli and Daman and Diu', 
+    603, 0.01
+  );
+/* INSERT QUERY */
+INSERT INTO states1(
+    statename, 
+    Area, 
+    nationalshare
+) 
+VALUES 
+  ('Puducherry', 492, 0.01);
+/* INSERT QUERY */
+INSERT INTO states1(
+    statename, 
+    Area, 
+    nationalshare
+) 
+VALUES 
+  ('Chandigarh', 114, 0.003);
---
+++ export_import.sql
@@ -0,0 +1,266 @@
+﻿/* CREATE TABLE */
+CREATE TABLE IF NOT EXISTS TABLE_NAME(
+  Rank INT(11), 
+  Country VARCHAR(100), 
+  Export VARCHAR(100), 
+  Import VARCHAR(100), 
+  Total_Trade VARCHAR(100), 
+  value VARCHAR(100)
+);
+/* INSERT QUERY */
+INSERT INTO TABLE_NAME(
+  Rank, Country, Export, Import, Total_Trade, 
+  value
+) 
+VALUES 
+  (
+    1, 'U S A ', '1,55,544.82', '1,14,655.89', 
+    '2,70,200.71', '40,888.93'
+  );
+/* INSERT QUERY */
+INSERT INTO TABLE_NAME(
+  Rank, Country, Export, Import, Total_Trade, 
+  value
+) 
+VALUES 
+  (
+    2, 'CHINA P RP ', '49,414.03', '2,07,994.15', 
+    '2,57,408.18', '-1,58,580.13'
+  );
+/* INSERT QUERY */
+INSERT INTO TABLE_NAME(
+  Rank, Country, Export, Import, Total_Trade, 
+  value
+) 
+VALUES 
+  (
+    3, 'U ARAB EMTS ', '88,303.69', '82,150.62', 
+    '1,70,454.31', '6,153.07'
+  );
+/* INSERT QUERY */
+INSERT INTO TABLE_NAME(
+  Rank, Country, Export, Import, Total_Trade, 
+  value
+) 
+VALUES 
+  (
+    4, 'SAUDI ARAB ', '15,967.08', '82,006.54', 
+    '97,973.62', '-66,039.46'
+  );
+/* INSERT QUERY */
+INSERT INTO TABLE_NAME(
+  Rank, Country, Export, Import, Total_Trade, 
+  value
+) 
+VALUES 
+  (
+    5, 'HONG KONG ', '32,449.92', '49,729.85', 
+    '82,179.77', '-17,279.94'
+  );
+/* INSERT QUERY */
+INSERT INTO TABLE_NAME(
+  Rank, Country, Export, Import, Total_Trade, 
+  value
+) 
+VALUES 
+  (
+    6, 'SINGAPORE ', '33,416.96', '45,014.10', 
+    '78,431.06', '-11,597.14'
+  );
+/* INSERT QUERY */
+INSERT INTO TABLE_NAME(
+  Rank, Country, Export, Import, Total_Trade, 
+  value
+) 
+VALUES 
+  (
+    7, 'IRAQ ', '5,807.83', '69,466.19', 
+    '75,274.02', '-63,658.36'
+  );
+/* INSERT QUERY */
+INSERT INTO TABLE_NAME(
+  Rank, Country, Export, Import, Total_Trade, 
+  value
+) 
+VALUES 
+  (
+    8, 'SWITZERLAND ', '3,793.90', '64,648.20', 
+    '68,442.10', '-60,854.29'
+  );
+/* INSERT QUERY */
+INSERT INTO TABLE_NAME(
+  Rank, Country, Export, Import, Total_Trade, 
+  value
+) 
+VALUES 
+  (
+    9, 'GERMANY ', '24,826.03', '41,044.21', 
+    '65,870.24', '-16,218.18'
+  );
+/* INSERT QUERY */
+INSERT INTO TABLE_NAME(
+  Rank, Country, Export, Import, Total_Trade, 
+  value
+) 
+VALUES 
+  (
+    10, 'KOREA RP ', '13,337.39', '50,075.34', 
+    '63,412.73', '-36,737.95'
+  );
+/* INSERT QUERY */
+INSERT INTO TABLE_NAME(
+  Rank, Country, Export, Import, Total_Trade, 
+  value
+) 
+VALUES 
+  (
+    11, 'INDONESIA ', '11,287.80', '41,825.67', 
+    '53,113.47', '-30,537.88'
+  );
+/* INSERT QUERY */
+INSERT INTO TABLE_NAME(
+  Rank, Country, Export, Import, Total_Trade, 
+  value
+) 
+VALUES 
+  (
+    12, 'JAPAN ', '13,683.04', '38,702.30', 
+    '52,385.34', '-25,019.26'
+  );
+/* INSERT QUERY */
+INSERT INTO TABLE_NAME(
+  Rank, Country, Export, Import, Total_Trade, 
+  value
+) 
+VALUES 
+  (
+    13, 'MALAYSIA ', '17,847.71', '31,607.12', 
+    '49,454.83', '-13,759.41'
+  );
+/* INSERT QUERY */
+INSERT INTO TABLE_NAME(
+  Rank, Country, Export, Import, Total_Trade, 
+  value
+) 
+VALUES 
+  (
+    14, 'U K ', '25,884.44', '21,058.47', 
+    '46,942.91', '4,825.97'
+  );
+/* INSERT QUERY */
+INSERT INTO TABLE_NAME(
+  Rank, Country, Export, Import, Total_Trade, 
+  value
+) 
+VALUES 
+  (
+    15, 'BELGIUM ', '16,161.88', '26,361.85', 
+    '42,523.73', '-10,199.97'
+  );
+/* INSERT QUERY */
+INSERT INTO TABLE_NAME(
+  Rank, Country, Export, Import, Total_Trade, 
+  value
+) 
+VALUES 
+  (
+    16, 'AUSTRALIA ', '8,408.64', '34,000.48', 
+    '42,409.12', '-25,591.83'
+  );
+/* INSERT QUERY */
+INSERT INTO TABLE_NAME(
+  Rank, Country, Export, Import, Total_Trade, 
+  value
+) 
+VALUES 
+  (
+    17, 'NIGERIA ', '9,748.20', '30,847.88', 
+    '40,596.08', '-21,099.68'
+  );
+/* INSERT QUERY */
+INSERT INTO TABLE_NAME(
+  Rank, Country, Export, Import, Total_Trade, 
+  value
+) 
+VALUES 
+  (
+    18, 'VIETNAM SOC REP ', '14,255.48', 
+    '25,312.29', '39,567.77', '-11,056.82'
+  );
+/* INSERT QUERY */
+INSERT INTO TABLE_NAME(
+  Rank, Country, Export, Import, Total_Trade, 
+  value
+) 
+VALUES 
+  (
+    19, 'NETHERLAND ', '27,934.71', '10,865.65', 
+    '38,800.36', '17,069.06'
+  );
+/* INSERT QUERY */
+INSERT INTO TABLE_NAME(
+  Rank, Country, Export, Import, Total_Trade, 
+  value
+) 
+VALUES 
+  (
+    20, 'THAILAND ', '12,389.36', '22,370.77', 
+    '34,760.13', '-9,981.41'
+  );
+/* INSERT QUERY */
+INSERT INTO TABLE_NAME(
+  Rank, Country, Export, Import, Total_Trade, 
+  value
+) 
+VALUES 
+  (
+    21, 'SOUTH AFRICA ', '12,150.86', 
+    '19,304.48', '31,455.35', '-7,153.62'
+  );
+/* INSERT QUERY */
+INSERT INTO TABLE_NAME(
+  Rank, Country, Export, Import, Total_Trade, 
+  value
+) 
+VALUES 
+  (
+    22, 'FRANCE ', '15,101.99', '15,982.90', 
+    '31,084.89',-880.91
+  );
+/* INSERT QUERY */
+INSERT INTO TABLE_NAME(
+  Rank, Country, Export, Import, Total_Trade, 
+  value
+) 
+VALUES 
+  (
+    23, 'RUSSIA ', '7,988.59', '21,943.13', 
+    '29,931.72', '-13,954.55'
+  );
+/* INSERT QUERY */
+INSERT INTO TABLE_NAME(
+  Rank, Country, Export, Import, Total_Trade, 
+  value
+) 
+VALUES 
+  (
+    24, 'QATAR ', '3,472.59', '25,481.61', 
+    '28,954.20', '-22,009.02'
+  );
+/* INSERT QUERY */
+INSERT INTO TABLE_NAME(
+  Rank, Country, Export, Import, Total_Trade, 
+  value
+) 
+VALUES 
+  (
+    25, 'ITALY ', '14,091.20', '14,288.35', 
+    '28,379.55',-197.15
+  );
+/* INSERT QUERY */
+INSERT INTO TABLE_NAME(
+  Rank, Country, Export, Import, Total_Trade, 
+  value
+) 
+VALUES 
+  ('');
